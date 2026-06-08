7d9700061b6bafaf61e0e2d26cd6849cfc484695
reychelbogantes <reychelfallasbogantes@gmail.com>
2026-06-07 22:58:21 -0600

    cambiosen el main

--- front_end_zfll/src/main.jsx
+++ front_end_zfll/src/main.jsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google';
-
+import { IS_MOCK } from './Services/mockAuth.js';
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const app = IS_MOCK ? (
+  <StrictMode>
+    <App />
+  </StrictMode>
+) : (
   <StrictMode>
     <GoogleOAuthProvider clientId="283271970934-353ubog56mdvg8q0i2aoted2pkmanib5.apps.googleusercontent.com">
-    <App />
+      <App />
     </GoogleOAuthProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+createRoot(document.getElementById('root')).render(app);
